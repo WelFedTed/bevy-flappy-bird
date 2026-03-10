2b6409d18dd4a6992eb130f7cd7170c4be96b195
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2026-03-11 08:05:44 +1100

    set resolution to 288x512 as per game assets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use bevy::prelude::*;
+use bevy::window::{WindowPlugin, WindowResolution};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "bevy-flappy-bird".into(),
+                resolution: WindowResolution::new(288, 512),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Update, hello_world)
         .run();
 }
